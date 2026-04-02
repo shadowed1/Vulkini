1c19781e1f219f37573dfa05cfd7cd2733fee1fc
shadowed1 <itszachg@gmail.com>
2026-04-01 20:58:27 -0400

    Update vulkini_pip.sh

--- vulkini_pip.sh
+++ vulkini_pip.sh
@@ -5,19 +5,17 @@ if ! dpkg -s python${PY_VER}-venv >/dev/null 2>&1; then
     sudo apt update
     sudo apt install -y python${PY_VER}-venv
 fi
-if [ ! -f "$VENV/bin/activate" ]; then
-    echo "[vulkini_meson] Creating venv at $VENV"
+if [ ! -d "$VENV" ]; then
+    echo "[vulkini_pip] Creating venv at $VENV"
     python3 -m venv "$VENV"
     source "$VENV/bin/activate"
-    echo "Installing meson + ninja"
     pip install --upgrade pip
-    pip install meson ninja mako pyyaml ply
-else
-    source "$VENV/bin/activate"
-fi
-
-"$@"
-
-if command -v deactivate >/dev/null 2>&1; then
+    pip install meson ninja mako pyyaml ply MarkupSafe
     deactivate
 fi
+if [ "$1" = "activate" ]; then
+    exec bash --rcfile <(echo "source $VENV/bin/activate")
+fi
+source "$VENV/bin/activate"
+"$@"
+deactivate
