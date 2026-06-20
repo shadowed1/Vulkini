2febc42e424830313b147f92959968078b93daa1
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-20 15:15:24 -0400

    Update install_mc.sh

--- install_mc.sh
+++ install_mc.sh
@@ -80,6 +80,7 @@ echo "${RESET}"
 sudo apt install flatpak -y
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.prismlauncher.PrismLauncher -y
+flatpak override --user --nosocket=wayland org.prismlauncher.PrismLauncher
 sudo curl -fsSL "https://raw.githubusercontent.com/shadowed1/Chard/main/bin/vulkan_tester.sh" -o "/bin/vulkan_tester" 2>/dev/null
 sleep 0.2
 sudo chmod +x /bin/vulkan_tester 2>/dev/null
